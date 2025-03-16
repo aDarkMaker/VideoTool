7da763624e257501096b03ed439db66fea3848b0
2261265112@qq.com <2261265112@qq.com>
2025-03-16 20:58:25 +0800

    init

--- Home.py
+++ Home.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import time
 
-st.markdown("# Home 🏠​")
-st.sidebar.markdown("# Home🏠​")
+st.subheader("# Home 🏠​")
+st.sidebar.subheader("# Home🏠​")
 
 st.text_input("Input", key="path-input")
 st.text_input("Output", key="path-output")
